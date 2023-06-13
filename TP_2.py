b9a4c8f85e38b3a5cb1c509d5f443754ac2e9b49
StoBrna <santinobrenna110@gmail.com>
2023-06-13 20:37:12 -0300

    hola

--- TP_2.py
+++ TP_2.py
@@ -1,7 +1,11 @@
 open('peajes.txt', "r").read()
 
+letters = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+digits = ('', "0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+
 def leer_idioma():
-    with open('peajes.txt', 'rt') as open:
+    with open('peajes.txt', 'rt') as m:
         line = m.readline()
         if line[8] == "P":
             idioma = "Portugués"
@@ -10,6 +14,90 @@ def leer_idioma():
 
     return idioma
 
+def identificar_pais():
+    argentina = 0
+    brazil = 0
+    uruguay = 0
+    paraguay = 0
+    bolivia = 0
+    chile = 0
+
+    if len(patent) != 7:
+        None
+
+    else:
+        if patent[0] in digits:
+            chile += 1
+
+        else:
+            argentina += 1
+            brazil += 1
+            uruguay += 1
+            paraguay += 1
+            bolivia += 1
+
+        if patent[1] in digits:
+            None
+
+        else:
+            argentina += 1
+            brazil += 1
+            uruguay += 1
+            paraguay += 1
+            bolivia += 1
+            chile += 1
+
+        if patent[2] in digits:
+            argentina += 1
+            bolivia += 1
+
+        else:
+            brazil += 1
+            uruguay += 1
+            paraguay += 1
+            chile += 1
+
+        if patent[3] in digits:
+            argentina += 1
+            brazil += 1
+            uruguay += 1
+            bolivia += 1
+
+        else:
+            paraguay += 1
+            chile += 1
+
+        if patent[4] in digits:
+            argentina += 1
+            uruguay += 1
+            paraguay += 1
+            bolivia += 1
+            chile += 1
+
+        else:
+            brazil += 1
+
+        if patent[5] in digits:
+            brazil += 1
+            uruguay += 1
+            paraguay += 1
+            bolivia += 1
+
+        else:
+            argentina += 1
+            chile += 1
+
+        #if chile == 6 and len(patent) == 6:
+            #None
+
+        if patent[6] in digits:
+            brazil += 1
+            uruguay += 1
+            paraguay += 1
+            bolivia += 1
+        else:
+            argentina += 1
+            chile += 1
 
 '''
  code = int(line[0:4])
@@ -24,10 +112,11 @@ def leer_idioma():
 '''
 
 
+
 def principal():
     print('(r1) - Idioma a usar en los informes:', idioma)
     print()
-
+idioma = leer_idioma()
 
 principal()
 
@@ -51,6 +140,4 @@ principal()
     print('(r6) - Porcentaje de patentes de otros países:', porc, '\b%')
     print()
     print('(r7) - Distancia promedio recorrida por vehículos argentinos pasando por
-    cabinas brasileñas:', prom, '\bkm')
-
-''
+    cabinas brasileñas:', prom, '\bkm')'''
